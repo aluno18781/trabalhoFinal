7eff1563dad01d8bb1006c6cefeca2dfeb4b1477
Pedro Tapadas <aluno18781@ipt.pt>
2019-04-19 18:41:52 +0100

    Criação do projeto + adição dos models e suas variaveis

---
+++ noticiasAuto/noticiasAuto/Models/Noticias.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace noticiasAuto.Models
+{
+    public class Noticias
+    {
+        public Noticias()
+        {
+            ListaDeEquipas = new HashSet<Equipas>();
+            ListaDeComentarios = new HashSet<Comentarios>();
+        }
+
+        public int IdNoticia { get; set; }
+
+        public string Fotografia { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Conteudo { get; set; }
+
+        [ForeignKey("Utilizadores")]
+        public int UserFK { get; set; }
+        public virtual Utilizadores Utilizadores { get; set; }
+
+        public virtual ICollection<Equipas> ListaDeEquipas{ get; set; }
+
+        public virtual ICollection<Comentarios> ListaDeComentarios { get; set; }
+
+
+    }
+}
