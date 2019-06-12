965b986c0ca529c00a3ad8b603300b0d9037c5c5
Pedro Tapadas <aluno18781@ipt.pt>
2019-06-12 11:51:27 +0100

    Instalação do package Identity samples

--- noticiasAuto/noticiasAuto/Controllers/utilizadoresController.cs
+++ noticiasAuto/noticiasAuto/Controllers/utilizadoresController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -11,126 +10,109 @@ using noticiasAuto.Models;
 
 namespace noticiasAuto.Controllers
 {
-    public class EquipasController : Controller
+    public class utilizadoresController : Controller
     {
         private NoticiasDB db = new NoticiasDB();
 
-        // GET: Equipas
+        // GET: utilizadores
         public ActionResult Index()
         {
-            return View(db.Equipas.ToList());
+            return View(db.Utilizadores.ToList());
         }
 
-        // GET: Equipas/Details/5
+        // GET: utilizadores/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Equipas equipas = db.Equipas.Find(id);
-            if (equipas == null)
+            utilizadores utilizadores = db.Utilizadores.Find(id);
+            if (utilizadores == null)
             {
                 return HttpNotFound();
             }
-            return View(equipas);
+            return View(utilizadores);
         }
 
-        // GET: Equipas/Create
+        // GET: utilizadores/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Equipas/Create
+        // POST: utilizadores/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "IdEquipa,Nome,DataFundacao,Logo,Fundador,Nacionalidade")] Equipas equipas)
+        public ActionResult Create([Bind(Include = "IdUser,Nome,Email")] utilizadores utilizadores)
         {
             if (ModelState.IsValid)
             {
-                db.Equipas.Add(equipas);
+                db.Utilizadores.Add(utilizadores);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(equipas);
+            return View(utilizadores);
         }
 
-        // GET: Equipas/Edit/5
+        // GET: utilizadores/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Equipas equipas = db.Equipas.Find(id);
-            if (equipas == null)
+            utilizadores utilizadores = db.Utilizadores.Find(id);
+            if (utilizadores == null)
             {
                 return HttpNotFound();
             }
-            return View(equipas);
+            return View(utilizadores);
         }
 
-        // POST: Equipas/Edit/5
+        // POST: utilizadores/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "IdEquipa,Nome,DataFundacao,Logo,Fundador,Nacionalidade")] Equipas equipas)
+        public ActionResult Edit([Bind(Include = "IdUser,Nome,Email")] utilizadores utilizadores)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(equipas).State = EntityState.Modified;
+                db.Entry(utilizadores).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(equipas);
+            return View(utilizadores);
         }
 
-        // GET: Equipas/Delete/5
+        // GET: utilizadores/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Equipas equipas = db.Equipas.Find(id);
-            if (equipas == null)
+            utilizadores utilizadores = db.Utilizadores.Find(id);
+            if (utilizadores == null)
             {
                 return HttpNotFound();
             }
-            return View(equipas);
+            return View(utilizadores);
         }
 
-        // POST: Equipas/Delete/5
+        // POST: utilizadores/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            //procurar a Equipa
-            Equipas equipa = db.Equipas.Find(id);
-
-            try
-            {
-                //remover da memória
-                db.Equipas.Remove(equipa);
-                //commit na BD
-                db.SaveChanges();
-                System.IO.File.Delete(Path.Combine(Server.MapPath("~/Imagens/"), equipa.Logo));
-                return RedirectToAction("Index");
-
-            }
-            catch (Exception)
-            {
-                //gerar uma mensagem de erro, a ser aprentada ao utilizador
-                ModelState.AddModelError("", string.Format("Não foi possível remover a Equipa, porque existem notícias associadas a ela.", id));
-                //reenviar os dados para a View
-            }
-            return View(equipa);
-
+            utilizadores utilizadores = db.Utilizadores.Find(id);
+            db.Utilizadores.Remove(utilizadores);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
